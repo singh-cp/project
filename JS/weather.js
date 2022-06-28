c6f65e91855487878229c1fa1f8d0a1288080020
singh-cp <chandraprakash.iam@gmail.com>
2022-06-29 00:18:16 +0530

    weather

---
+++ JS/weather.js
@@ -0,0 +1,22 @@
+const weather = document.querySelector("#weather");
+
+function myGetWeather() {
+    navigator.geolocation.getCurrentPosition(extractDetails);
+}
+function extractDetails(details) {
+    let longitude = details.coords.longitude;
+    let latitude = details.coords.latitude;
+    weatherDetails(longitude, latitude);
+}
+
+function weatherDetails(lon, lat) {
+    const apiKEY = `8f2ff4a7b08410e66ce21250463bb166`
+    const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKEY}&units=metric`
+    const weatherAPI = fetch(URL, { method: 'GET' })
+        .then(res => res.json())
+        .then(data => {
+            weather.innerHTML = `
+            <h3>${data.name}</h3>
+            <h3>${data.main.temp}&deg; C</h3>`
+        });
+}
